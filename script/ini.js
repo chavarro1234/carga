b67fe02bba15ad0fe53857e14156564abaad5cc9
chavarro1234 <42097738+chavarro1234@users.noreply.github.com>
2022-08-20 12:45:47 -0500

    Add files via upload

---
+++ script/ini.js
@@ -0,0 +1,26 @@
+function ir(){
+
+    var u="vitobisxddd@gmail.com";
+    var c="Camilo123";
+    if(document.form.pass.value==c && document.form.login.value==u){
+    alert ("Bienviendio a De Todo Pelis");
+    window.location="go: Inicio";}
+    
+    else{
+        alert("Contraseña O Usuario Incorrecto");
+    }
+    
+    
+    }
+
+    function ir(){
+ var u="camilo@gmail.com";
+var c="prueba1";
+if(document.form.pass.value==c && document.form.login.value==u){
+alert ("Bienviendio a De Todo Pelis");
+window.location="go: inicio";}
+    
+    else{
+        alert("Contraseña O Usuario Incorrecto");
+    }
+}
