31bad74b82dd1b9e693cf09a0b8f3c67b9f4eb27
chavarro1234 <42097738+chavarro1234@users.noreply.github.com>
2022-08-20 09:49:19 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+function ir(){
+
+var c="dara17";
+var u="Peligro";
+
+if(document.form.password.value==c && document.form.username.value==u){
+alert ("Bienvenido A Comunidad Hacker");
+window.location="index.html";}
+
+
+else{
+alert("Contraseña o Usuario Incorrecto");
+}
+
+
+
+
+
+
+
+}
